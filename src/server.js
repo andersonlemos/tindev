db42f85260e621cd7d6596fa1b912a00a95f6006
Anderson Lemos <bdl182@openmailbox.org>
2019-09-02 23:17:51 -0300

    last update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors =  require('cors');
 const winston = require('winston');
-const server  = express();
+
+const app  = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const routes = require('./routes');
 const database = 'mongodb+srv://omnistack:omnistack@cluster0-3vaz8.mongodb.net/omnistack8?retryWrites=true&w=majority';
 
@@ -14,11 +18,24 @@ const logger = winston.createLogger({
       new winston.transports.File({ filename: 'combined.log' })
     ]
   });
+  const connectedUsers = {};
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+  io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+    console.log(user,socket.id);
+    connectedUsers[user] = socket.id;
+  });
 
-server.listen(3333, () => {
+  app.use((req, res, next) =>{
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    
+    return next();
+  });
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+
+  server.listen(3333, () => {
     console.log('Server Running on 3333');
 })
